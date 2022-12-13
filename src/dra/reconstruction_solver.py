1c45bb081fd54b59a534882454da73d9d0cc117a
mitch-tw <mitchell.lisle@thoughtworks.com>
2022-12-13 11:47:07 +1100

    updates to reconstruction

--- src/dra/reconstruction_solver.py
+++ src/dra/reconstruction_solver.py
@@ -3,6 +3,19 @@ from typing import Callable, Tuple, Union
 import pandas as pd
 import z3
 
+database = pd.DataFrame(
+    [
+        ('Sara Gray', 8, False, False, False),
+        ('Joseph Collins', 18, False, True, True),
+        ('Vincent Porter', 24, True, False, True),
+        ('Tiffany Brown', 30, True, True, True),
+        ('Brenda Small', 36, True, False, False),
+        ('Dr. Tina Ayala', 66, True, False, False),
+        ('Rodney Gonzalez', 84, True, True, False),
+    ],
+    columns=['name', 'age', 'married', 'smoker', 'employed'],
+)
+
 stats = pd.DataFrame(
     [
         {'id': 'A1', 'name': 'total-population', 'count': 7, 'mean': 38.0, 'median': 30.0},
