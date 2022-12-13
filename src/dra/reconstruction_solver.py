2005f7c4d4ba1045c219314dd475a84b1cbb2eb3
mitch-tw <mitchell.lisle@thoughtworks.com>
2022-12-13 11:35:25 +1100

    updates to reconstruction

--- src/dra/reconstruction_solver.py
+++ src/dra/reconstruction_solver.py
@@ -187,3 +187,38 @@ def _add_employment_constraints(
     )
 
     return solver, employed_indices, unemployed_indices
+
+
+def model_as_dataframe(
+    model, *, ages, married_indices, smoker_indices, employed_indices
+) -> pd.DataFrame:
+    if isinstance(model, z3.ModelRef):
+        df = pd.DataFrame(
+            {
+                'age': [model.evaluate(z3.Select(ages, i)) for i in range(7)],
+                'married': [i in [model[idx] for idx in married_indices] for i in range(7)],
+                'smoker': [i in [model[idx] for idx in smoker_indices] for i in range(7)],
+                'employed': [i in [model[idx] for idx in employed_indices] for i in range(7)],
+            }
+        )
+
+        return df
+    raise TypeError(
+        'model is not initialised. Either there is no valid model, '
+        'or you forgot to run DatabaseReconstructionAttack.run?'
+    )
+
+
+def check_accuracy(output, database) -> float:
+    match, non_match = 0, 0
+    computed = [tuple(v.values()) for v in output.to_dict(orient='records')]  # type: ignore
+    original = [tuple(v.values()) for v in database.to_dict(orient='records')]  # type: ignore
+
+    to_check = [list(zip(computed[i], original[i])) for i in range(7)]
+    for items in to_check:
+        for pair in items:
+            if pair[0] == pair[1]:
+                match += 1
+            else:
+                non_match += 1
+    return (match / (match + non_match)) * 100
--- tests/test_reconstruction_solver.py
+++ tests/test_reconstruction_solver.py
@@ -3,7 +3,7 @@ from itertools import tee
 import pandas as pd
 import z3
 
-from dra.reconstruction_solver import reconstruction
+from dra.reconstruction_solver import check_accuracy, model_as_dataframe, reconstruction
 
 
 def pairwise(iterable):
@@ -28,8 +28,8 @@ database = pd.DataFrame(
 
 def add_min_max_constraint(solver: z3.Solver, ages: z3.Array) -> z3.Solver:
     for i in range(7):
-        solver.add(ages[i] > 0)
-        solver.add(ages[i] < 100)
+        solver.add(ages[i] >= 0)
+        solver.add(ages[i] <= 125)
 
     return solver
 
@@ -49,41 +49,6 @@ def add_pairwise_sort_constraint(solver: z3.Solver, ages: z3.Array) -> z3.Solver
 def add_population_median(solver: z3.Solver, ages: z3.Array) -> z3.Solver:
     solver.add(z3.Select(ages, 3) == 30)
     return solver
-
-
-def check_accuracy(output, database) -> float:
-    match, non_match = 0, 0
-    computed = [tuple(v.values()) for v in output.to_dict(orient='records')]  # type: ignore
-    original = [tuple(v.values()) for v in database.to_dict(orient='records')]  # type: ignore
-
-    to_check = [list(zip(computed[i], original[i])) for i in range(7)]
-    for items in to_check:
-        for pair in items:
-            if pair[0] == pair[1]:
-                match += 1
-            else:
-                non_match += 1
-    return (match / (match + non_match)) * 100
-
-
-def model_as_dataframe(
-    model, *, ages, married_indices, smoker_indices, employed_indices
-) -> pd.DataFrame:
-    if isinstance(model, z3.ModelRef):
-        df = pd.DataFrame(
-            {
-                'age': [model.evaluate(z3.Select(ages, i)) for i in range(7)],
-                'married': [i in [model[idx] for idx in married_indices] for i in range(7)],
-                'smoker': [i in [model[idx] for idx in smoker_indices] for i in range(7)],
-                'employed': [i in [model[idx] for idx in employed_indices] for i in range(7)],
-            }
-        )
-
-        return df
-    raise TypeError(
-        'model is not initialised. Either there is no valid model, '
-        'or you forgot to run DatabaseReconstructionAttack.run?'
-    )
 
 
 def test_reconstruction():
