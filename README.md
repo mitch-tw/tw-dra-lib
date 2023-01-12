268090382192f1eafd955400d8a861411e1db306
mitch-tw <mitchell.lisle@thoughtworks.com>
2023-01-12 11:34:35 +1100

    Bump version: 0.18.0 → 0.19.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ##  dra
 
-> 1️⃣ version: 0.18.0
+> 1️⃣ version: 0.19.0
 
 > ✍️ author: Mitchell Lisle
 
--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setup(
     license='Not open source',
     tests_require=test_requirements,
     url='https://github.com/mitch-tw/dra',
-    version='0.18.0',
+    version='0.19.0',
     zip_safe=False,
 )
